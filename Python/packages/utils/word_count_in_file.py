33cb773a34259a2fd6e5fda210bb232b1a6cdd21
joey <neigie@gmail.com>
2016-01-24 19:59:52 +0900

    make input/outputfile name to parameters of function

--- Python/packages/utils/word_count_in_file.py
+++ Python/packages/utils/word_count_in_file.py
@@ -1,22 +1,34 @@
 import time
 from collections import Counter
 
-def count():
+def count(inputfile, outputfile):
     cnt = Counter()
 
     print("start..")
     before = time.time()
-    with open('input.txt', 'r') as rf:
-        for line in rf:
-            for word in line.split(' '):
-                cnt[word] += 1
+    try:
+        with open(inputfile, 'r') as rf:
+            for line in rf:
+                for word in line.split(' '):
+                    cnt[word] += 1
 
-    with open('output.txt', 'w') as wf:
-        for k, v in cnt.most_common():
-            wf.write("{} {}\n".format(k, v))
+        with open(outputfile, 'w') as wf:
+            for k, v in cnt.most_common():
+                wf.write("{} {}\n".format(k, v))
+
+    except FileNotFoundError:
+        print("invalid inputfile", inputfile)
 
     after = time.time()
-    print("done... elapse time: " + str(after-before) + " sec")
+    print("done... elapse time: {0:2.2f} sec".format(after-before))
 
 if __name__ == "__main__":
-    count()
+    inputfile = input("insert input file name: (default: input.txt)")
+    outputfile = input("insert output file name: (default: output.txt)")
+
+    if len(inputfile) == 0:
+        inputfile = 'input.txt'
+    if len(outputfile) == 0:
+        outputfile = 'output.txt'
+        
+    count(inputfile, outputfile)

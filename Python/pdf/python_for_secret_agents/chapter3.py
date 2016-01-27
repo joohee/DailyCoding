dad50163e1e83362aaf73572deaa2e59b0bcafa1
Joohee Kang <neigie@gmail.com>
2016-01-27 17:32:13 +0900

    chapter3 example file added

---
+++ Python/pdf/python_for_secret_agents/chapter3.py
@@ -0,0 +1,40 @@
+import glob
+import os
+
+def example1():
+    pictures = glob.glob("*.jpg")
+    print(pictures)
+
+    for picture in pictures:
+        print(os.path.splitext(picture))
+    
+def example2():
+    with open("chapter2.py") as data:
+        clean_lines = (raw.rstrip() for raw in data)
+        for line in clean_lines:
+            print(line)
+
+        print("-----")
+
+        non_blank_lines = (line for line in clean_lines if len(line) != 0)
+        for line in non_blank_lines:
+            print(line)
+
+def example3():
+    import zipfile
+    with zipfile.ZipFile("demo.zip", "r") as archive:
+        #archive.printdir()
+            first = archive.infolist()[0]
+            with archive.open(first) as member:
+                text = member.read()
+                print(text)
+
+            for filename in archive.namelist():
+                print(filename)
+                archive.extract(filename)
+
+
+if __name__ == "__main__":
+    #example1()
+    #example2()
+    example3()

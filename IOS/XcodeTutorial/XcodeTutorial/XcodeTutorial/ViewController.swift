73d2b098b086b200ff438976bdc4ca58e5883ef7
joey <neigie@gmail.com>
2016-02-21 15:56:49 +0900

    swift project initial commit

---
+++ IOS/XcodeTutorial/XcodeTutorial/XcodeTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeTutorial
+//
+//  Created by Joohee Kang on 2016. 2. 19..
+//  Copyright © 2016년 Joohee Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

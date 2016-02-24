18998d1eeb49b494cbe6f68ab54beba038b3e97b
joey <neigie@gmail.com>
2016-02-24 10:48:26 +0900

    changed a part of image initialization

--- IOS/XcodeTutorial/XcodeTutorial/XcodeTutorial/RatingControl.swift
+++ IOS/XcodeTutorial/XcodeTutorial/XcodeTutorial/RatingControl.swift
@@ -2,17 +2,42 @@ import UIKit
 
 class RatingControl: UIView {
     
+    // MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
     // MARK: Initialization 
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y:0, width:44, height:44))
-        button.backgroundColor = UIColor.redColor()
-        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
-        addSubview(button)
+        let filledStartImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
         
-        print("initialize...")
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.setImage(emptyStarImage, forState: .Normal)
+            button.setImage(filledStartImage, forState: .Selected)
+            button.setImage(filledStartImage, forState: [.Highlighted, .Selected])
+            button.adjustsImageWhenDisabled = false     // 버튼 상태 변경시의 별도의 하이라이트 효과가 나타나는 것을 막아줌. 
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+        
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+        
+        
     }
 
     override func intrinsicContentSize() -> CGSize {

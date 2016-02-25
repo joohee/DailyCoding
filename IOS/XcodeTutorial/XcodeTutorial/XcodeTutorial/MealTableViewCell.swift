ace5b95e1bd6fde1b26b02a04b7baacdd55974b7
joey <neigie@gmail.com>
2016-02-25 14:41:32 +0900

    UITableViewController 및 Custom TableViewCell 추가

---
+++ IOS/XcodeTutorial/XcodeTutorial/XcodeTutorial/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  XcodeTutorial
+//
+//  Created by Joohee Kang on 2016. 2. 25..
+//  Copyright © 2016년 Joohee Kang. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

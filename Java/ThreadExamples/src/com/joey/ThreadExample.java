848ad05a75dd6af0845a2c7d3641d61faa81e598
Joohee Kang <neigie@gmail.com>
2016-02-26 14:57:25 +0900

    add Java example related Thread

---
+++ Java/ThreadExamples/src/com/joey/ThreadExample.java
@@ -0,0 +1,33 @@
+package com.joey;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadExample {
+
+    private static final Executor executor = Executors.newFixedThreadPool(3, new ThreadFactory() {
+        private AtomicInteger num = new AtomicInteger();
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r, "thread-" + num.getAndIncrement());
+            t.setDaemon(false);
+            t.setPriority(Thread.NORM_PRIORITY);
+            t.setUncaughtExceptionHandler((t1, e) -> {
+                System.out.println("error occured..." +  t1.getName() + ", " + e.getCause());
+                e.printStackTrace();
+            });
+            return t;
+        }
+    });
+
+    public static void main(String[] args) {
+        executor.execute(() -> {
+            throw new RuntimeException();
+        });
+    }
+
+
+
+}

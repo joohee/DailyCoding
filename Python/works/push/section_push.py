14fd4bae7f6243ab50bf61b79a275c10c5b379ed
Joohee Kang <neigie@gmail.com>
2016-03-03 15:23:24 +0900

    message randomize

--- Python/works/push/section_push.py
+++ Python/works/push/section_push.py
@@ -2,8 +2,10 @@ import boto3
 from sns import SNSObject
 import json
 import os, sys
+import random
 import send_push
 
 if __name__ == "__main__":
-    message = "test message"
-    send_push.push('section', message)
+    messages = ('test message1', 'test message2', 'test message3')
+    rand = random.randrange(0, len(messages))
+    send_push.push('section', messages[rand])

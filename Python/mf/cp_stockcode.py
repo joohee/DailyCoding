43d633ab931436ca4166ca30692f257ff9a9d85f
Joohee Kang <neigie@gmail.com>
2016-03-14 15:18:12 +0900

    soption API 추가

--- Python/mf/cp_stockcode.py
+++ Python/mf/cp_stockcode.py
@@ -22,13 +22,15 @@ if __name__ == '__main__':
     today = datetime.datetime.now().strftime('%y%m%d')
     instCpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
     instCpFutureCode = win32com.client.Dispatch("CpUtil.CpFutureCode")
-    instCpOptionCode = win32com.client.Dispatch("CpUtil.CpOptionCode")
     instCpKFutureCode = win32com.client.Dispatch("CpUtil.CpKFutureCode")
+    instCpOptionCode = win32com.client.Dispatch("CpUtil.CpOptionCode")
+    instCpSoptionCode = win32com.client.Dispatch("CpUtil.CpSOptionCode")
     instCpElwCode = win32com.client.Dispatch("CpUtil.CpElwCode")
 
     get_code_name(instCpStockCode, today, 'stock_code_name.csv')
     get_code_name(instCpFutureCode, today, 'future_code_name.csv')
-    get_code_name(instCpOptionCode, today, 'option_code_name.csv', 5)
     get_code_name(instCpKFutureCode, today, 'kfuture_code_name.csv')
+    get_code_name(instCpOptionCode, today, 'option_code_name.csv', 5)
+    get_code_name(instCpSOptionCode, today, 'soption_code_name.csv', 5)
     get_code_name(instCpElwCode, today, 'elw_code_name.csv', 7)
 

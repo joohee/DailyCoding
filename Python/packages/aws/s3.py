3e5ca541022a4ca1de8e9c9e47a33c45cb1fea02
Joey <neigie@gmail.com>
2016-04-11 16:39:16 +0900

    add set_public() function

--- Python/packages/aws/s3.py
+++ Python/packages/aws/s3.py
@@ -14,6 +14,20 @@ class S3Object:
         data = open(filename, 'rb')
         self.s3.Bucket(bucket_name).put_object(Key=filename, Body=data)
 
+    def list(self, bucket_name, dirname):
+        fileobjs = []
+        filenames = []
+        bucket = self.s3.Bucket(bucket_name)
+        for key in bucket.objects.all():
+            if key.key.startswith(dirname):
+                fileobjs.append(key)
+                filenames.append(key.key)
+                #print(key.key)
+        return fileobjs, filenames
+    
+    def set_public(self, obj):
+        obj.Acl().put(ACL='public-read')
+        print("{} set be public...".format(obj.key))
+
 if __name__ == '__main__':
-    s3 = S3Object('profile_name')
-    s3.upload('test-bucket', 'test_file.txt')
+    print("use S3Object class")

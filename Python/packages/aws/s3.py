11bf84ba76329ae52567255d0c4ff9e240e56ef1
joey <neigie@gmail.com>
2016-04-17 12:27:15 +0900

    add docstring

--- Python/packages/aws/s3.py
+++ Python/packages/aws/s3.py
@@ -2,6 +2,20 @@ import boto3
 import json
 
 class S3Object:
+    '''boto3 모듈을 이용한 s3 access functions.
+        - upload(bucket_name, filename, ACL)
+            : 특정 버킷/특정 파일을 업로드 합니다. 
+              ACL의 default값은 public-read입니다.
+        - list(bucket_name, dirname)
+            : 특정 버킷 아래 특정 디렉토리의 파일object와 파일 명을 출력합니다. 
+        - set_public(object)
+            : 특정 s3 object의 ACL을 public-read로 생성합니다. 
+        - get_url(bucket_name, key, expiration=3600000, http_method=None)
+            : 특정 bucket아래 object의 key에 대해 
+              expiration(default 1시간) 만큼 유효한 S3 URL을 리턴합니다. 
+              http_method는 제한이 없으며, 특정 method로 제한할 수 있다. 
+              (ex. 'GET', 'POST', 'PUT', ...)
+    '''
 
     def __init__(self, profile_name):
         self.session = boto3.Session(profile_name=profile_name)

b0ac3f7a7d8659a6ea71640ab5e5a89acbfad4e5
joey <neigie@gmail.com>
2016-05-03 23:41:07 +0900

    add docstring

--- Python/test/unittest_expected_failure.py
+++ Python/test/unittest_expected_failure.py
@@ -1,6 +1,10 @@
 import unittest
 
 class ExpectedFailureTestCase(unittest.TestCase):
+    ''' fail할 것을 가정하고 생성한 function예제입니다.
+        
+        unittest.TestCase를 Arugment로 갖고 생성한 클래스의 메소드를 실행합니다.
+    '''
     
     @unittest.expectedFailure
     def test_fail(self):

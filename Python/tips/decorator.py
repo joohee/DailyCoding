5234010696212a907fe0b5680e238a2292614e22
joey <neigie@gmail.com>
2016-05-04 16:52:07 +0900

    add docstring

--- Python/tips/decorator.py
+++ Python/tips/decorator.py
@@ -1,6 +1,17 @@
 import time
 import datetime
 
+''' decorator (@...)를 이용하여 Java의 AOP 같은 역할을 할 수 있다.
+
+    decorator 로 elapsed_time() function을 등록하면, 
+    아래와 같은 결과가 나온다. 
+
+    ⇒  python decorator.py
+    start 2016-05-04 16:51:38
+    hello
+    end 2016-05-04 16:51:38
+    Elapsed time: 0.000106 sec.
+'''
 def elapsed_time(functor):
     def decorated():
         start = time.time()

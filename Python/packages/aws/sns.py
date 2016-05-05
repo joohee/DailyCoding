331b200c3f43ac6c54d60257616fc5ce42e2811b
joey <neigie@gmail.com>
2016-05-05 20:26:36 +0900

    add docstring & add text how to create doc html file

--- Python/packages/aws/sns.py
+++ Python/packages/aws/sns.py
@@ -4,13 +4,24 @@ import configparser
 import json
 
 class SNSObject:
+    """ Amazon SNS 모듈을 이용하여 Push Notification을 전송합니다. 
+
+        도움말을 html로 저장하려면 아래 명령어를 입력하세요.
+            pydoc -w sns
+
+    """
 
     def __init__(self):
+        """ 사용할 SNS 클라이언트는 계속 활용되므로 class 내부 변수로 생성합니다. 
+            default message는 호출한 쪽에서 메세지를 입력하지 않았을 때 사용합니다.  
+        """
         self.client = boto3.client('sns')
         self.default_message = 'default message'
 
     # todo : message invisible in app. check it.
     def get_push_message_gcm(self, msg):
+        """ GCM 포맷에 맞는 message를 JSON 타입으로 변환하여 리턴합니다. 
+        """
         text = msg if msg != None else self.default_message
         # publish
         dData = {}
@@ -36,6 +47,8 @@ class SNSObject:
         return message
 
     def get_push_message_apns(self, msg):
+        """ APNS 포맷에 맞는 message를 JSON 타입으로 변환하여 리턴합니다. 
+        """
         text = msg if msg != None else self.default_message
 
         message = {}
@@ -59,6 +72,9 @@ class SNSObject:
         return message
     
     def create_platform_application(self, client, platform, key, cer):
+        """ Push를 발송할 수 있는 어플리케이션을 생성합니다. 
+          이때 APNS는 Push인증서로 생성한 cer/key 파일이, GCM은 Google에서 발급한 serverKey값이 필요합니다. 
+        """
         apns_attributes = {
             "PlatformCredential": key,
             "PlatformPrincipal": cer
@@ -73,6 +89,9 @@ class SNSObject:
         return response
     
     def create_endpoint(self, client, platformApplicationArn, pushToken):
+        """ 생성한 platformApplication과 GCM.registraionId 혹은 APNS.pushToken을 이용하여 
+          Push를 전송할 endpoint를 생성하는 메소드입니다. 
+        """
         platformEndpoint = client.create_platform_endpoint(
             PlatformApplicationArn=platformApplicationArn,
             Token=pushToken,
@@ -83,6 +102,8 @@ class SNSObject:
         return platformEndpoint
     
     def publish(self, client, endpointArn, message):
+        """ endpoint로 메세지를 전송하는 역할을 담당합니다.
+        """
         response = client.publish(
             MessageStructure='json',
             Message=json.dumps(message),
@@ -91,18 +112,27 @@ class SNSObject:
         return response
     
     def delete_endpoint(self, client, endpointArn):
+        """ 위에서 생성한 endpoint를 삭제합니다. 
+        """
         deletedEndpoint = client.delete_endpoint(
             EndpointArn=endpointArn
         )
         return deletedEndpoint
     
     def delete_platform_application(self, client, platformApplicationArn):
+        """ 위에서 생성한 platformApplication을 삭제합니다. 
+        """
         deletedPlatformApplication = client.delete_platform_application(
             PlatformApplicationArn=platformApplicationArn
         )
         return deletedPlatformApplication
 
     def push_apns(self, client, pushToken, message=None):
+        """ APNS 플랫폼에 따라 push를 전송하는 메소드입니다. 
+          message는JSON 포맷으로 이루어지며,  
+          get_push_message_gcm, get_push_message_apns를 통해 
+          플랫폼에 맞는 JSON 형식으로 리턴됩니다. 
+        """
         key_str, cer_str, key_filename, cer_filename=('',)*4
 
         config = configparser.ConfigParser()
@@ -167,6 +197,12 @@ class SNSObject:
         print("delete platform application: {}".format(deletedPlatformApplication))
     
     def push_gcm(self, client, registrationId, message=None):
+        """
+          GCM 플랫폼에 따라 push를 전송하는 메소드입니다. 
+          message는JSON 포맷으로 이루어지며,  
+          get_push_message_gcm, get_push_message_apns를 통해 
+          플랫폼에 맞는 JSON 형식으로 리턴됩니다. 
+        """
     
         config = configparser.ConfigParser()
         config.read(os.path.abspath(os.path.join(os.path.dirname(__file__), 'sns_config.ini')))

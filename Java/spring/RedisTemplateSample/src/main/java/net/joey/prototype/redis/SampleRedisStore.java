273493dde963bbe7f1d2bdab2bc7f4b2f88d4fd1
Joey <neigie@gmail.com>
2016-05-19 09:35:55 +0900

    add function to save multiple tuples

--- Java/spring/RedisTemplateSample/src/main/java/net/joey/prototype/redis/SampleRedisStore.java
+++ Java/spring/RedisTemplateSample/src/main/java/net/joey/prototype/redis/SampleRedisStore.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -30,6 +31,13 @@ public class SampleRedisStore {
 
 	public boolean add(String key, String value)	{
 		this.writableTemplate.execute(new SaveOne(getKeyFor(key), value));
+		return true;
+	}
+
+	public boolean add(Map<String, String> multi) {
+		if (multi.size() == 0) return false;
+
+		this.writableTemplate.execute(new SaveMultiple(multi));
 		return true;
 	}
 
@@ -71,6 +79,27 @@ public class SampleRedisStore {
 		public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
 			operations.multi();
 			operations.opsForValue().set((K) key, (V)value, TTL_IN_SEC, TimeUnit.SECONDS);
+			return operations.exec();
+		}
+	}
+
+	private class SaveMultiple implements SessionCallback<List<Object>> {
+		private final Map<String, String> tuples;
+
+		public SaveMultiple(Map<String, String> tuples) {
+			this.tuples = tuples;
+		}
+
+		@Override
+		public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
+			// Mark the start of a transaction block. @see RedisTxCommands
+			operations.multi();
+			for (Map.Entry<String, String> entry : tuples.entrySet()) {
+				String key = getKeyFor(entry.getKey());
+				String value = entry.getValue();
+
+				operations.opsForValue().set((K)key, (V)value, TTL_IN_SEC, TimeUnit.SECONDS);
+			}
 			return operations.exec();
 		}
 	}

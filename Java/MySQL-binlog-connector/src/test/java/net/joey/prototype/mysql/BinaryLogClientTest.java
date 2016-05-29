f033e3249cfd1b18313fdf13303df2018187b26a
joey <neigie@gmail.com>
2016-05-29 22:46:14 +0900

    clean up test classes

--- Java/MySQL-binlog-connector/src/test/java/net/joey/prototype/mysql/BinaryLogClientTest.java
+++ Java/MySQL-binlog-connector/src/test/java/net/joey/prototype/mysql/BinaryLogClientTest.java
@@ -1,20 +1,6 @@
-/*
- * Copyright 2013 Stanley Shyiko
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.github.shyiko.mysql.binlog;
+package net.joey.prototype.mysql;
 
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
 import com.github.shyiko.mysql.binlog.jmx.BinaryLogClientStatistics;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -25,9 +11,6 @@ import java.util.concurrent.TimeoutException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-/**
- * @author <a href="mailto:stanley.shyiko@gmail.com">Stanley Shyiko</a>
- */
 @Slf4j
 public class BinaryLogClientTest {
 
@@ -37,13 +20,9 @@ public class BinaryLogClientTest {
         assertTrue(binaryLogClient.getEventListeners().isEmpty());
         TraceEventListener traceEventListener = new TraceEventListener();
         binaryLogClient.registerEventListener(traceEventListener);
-        binaryLogClient.registerEventListener(new CountDownEventListener());
-        binaryLogClient.registerEventListener(new CapturingEventListener());
-        assertEquals(binaryLogClient.getEventListeners().size(), 3);
-        binaryLogClient.unregisterEventListener(traceEventListener);
-        assertEquals(binaryLogClient.getEventListeners().size(), 2);
-        binaryLogClient.unregisterEventListener(CountDownEventListener.class);
         assertEquals(binaryLogClient.getEventListeners().size(), 1);
+        binaryLogClient.unregisterEventListener(traceEventListener);
+        assertEquals(binaryLogClient.getEventListeners().size(), 0);
     }
 
     @Test
@@ -75,14 +54,12 @@ public class BinaryLogClientTest {
 
     @Test
     public void testReadBinaryLog() {
-        BinaryLogClient client = new BinaryLogClient("localhost", 3306, "root", "mysql");
+        BinaryLogClient client = new BinaryLogClient("localhost", 3306, "root", "");
         try {
             client.registerLifecycleListener(new BinaryLogClientStatistics());
             client.registerLifecycleListener(new TraceLifecycleListener());
             TraceEventListener traceEventListener = new TraceEventListener();
             client.registerEventListener(traceEventListener);
-            client.registerEventListener(new CountDownEventListener());
-            client.registerEventListener(new CapturingEventListener());
             client.connect();
 
             String fileName = client.getBinlogFilename();
--- Java/MySQL-binlog-connector/src/test/java/net/joey/prototype/mysql/TraceLifecycleListener.java
+++ Java/MySQL-binlog-connector/src/test/java/net/joey/prototype/mysql/TraceLifecycleListener.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.shyiko.mysql.binlog;
+package net.joey.prototype.mysql;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
 

d3bc3b875995d3f7ad1ee873066b122e65ff23bf
joey <neigie@gmail.com>
2016-06-05 11:16:27 +0900

    remove unused classes

--- Java/MySQL-binlog-connector/src/main/java/net/joey/prototype/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package net.joey.prototype.repository;
-
-import net.joey.prototype.domain.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
